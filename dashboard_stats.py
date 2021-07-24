2389b38f24635579797a2253425980174a19b8e3
Gautham Anil <gautham.anil@gmail.com>
2021-07-24 07:22:47 +0000

    Merging stock data to dashboard stats

--- dashboard_stats.py
+++ dashboard_stats.py
@@ -14,22 +14,29 @@ from treldev import sql_repr, instance_ts_str_to_ts_precision
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--tweet_stats")
+    parser.add_argument("--stock_series_30day")
     parser.add_argument("--dashboard_stats")
     parser.add_argument("--_schedule_instance_ts")
     args, _ = parser.parse_known_args()
 
     output_bq = BigQueryURI(args.dashboard_stats)
     tweet_stats_bq = BigQueryURI(args.tweet_stats)
+    stock_series_30day_bq = BigQueryURI(args.stock_series_30day)
 
     output_bq.save_sql_results(f"""
 with 
 series as (SELECT * FROM `{tweet_stats_bq.path}`)
+,stock_series as (SELECT * FROM `{stock_series_30day_bq.path}`)
 ,max_ as (select cast("{args._schedule_instance_ts}" as datetime) max_ts)
-,recent as (select series.* from series cross join max_ where datetime_diff(max_ts, ts, HOUR) < 500)
-,older as (select series.* from series cross join max_ where datetime_diff(max_ts, ts, HOUR) >= 500)
-, older_agg as (select datetime(timestamp_seconds(cast(avg(unix_seconds(timestamp(ts))) as int64))) ts, avg(tweets) tweets, avg(verified_tweets) verified_tweets from older group by substr(cast(ts as string),1,13))
-,combined as (select * from older_agg UNION ALL select * from recent)
-,res as (select ts, * EXCEPT(ts) from combined order by ts desc limit 300)
 
-select * from series
-    """)
+
+,stock_series_5mins as (select *
+  from stock_series)
+,joined as (select 
+  datetime(timestamp(coalesce(series.ts, stock_series.ts)),"America/New_York") ts_ny, 
+  coalesce(series.ts, stock_series.ts) ts, 
+  series.* EXCEPT(ts), stock_series. * EXCEPT (ts) 
+from series full outer join stock_series on series.ts = stock_series.ts)
+
+select * from joined order by ts desc
+""")
