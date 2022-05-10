827cde78489017b9537f4a05a47c382f13f094b4
Gautham Anil <gautham.anil@gmail.com>
2022-05-10 20:21:41 +0000

    Small fix

--- push_to_bq_view.py
+++ push_to_bq_view.py
@@ -6,12 +6,12 @@ def main(inputs, outputs, schedule_instance_ts, credentials, **kwargs):
 
     assert len(inputs) == 1 # Only one input dataset class can be provided.
     input_datasets = list(inputs.values())[0]
-    assert len(inputs_datasets) == 2 # This should have only two datasets. One to be pushed, and one the latest.
+    assert len(input_datasets) == 2 # This should have only two datasets. One to be pushed, and one the latest.
     if len(set(v['uri'] for v in input_datasets)) == 2: # if they are different, do nothing. This way, we only push the latest.
         print("Not pushing path to view")
         return
 
-    input_bq_uri = BigQueryURI(inputs_datasets[0]['uri'])
+    input_bq_uri = BigQueryURI(input_datasets[0]['uri'])
     view_bq_uri = BigQueryURI(list(outputs.values())[0][0]['uri'])
     print(f"Pushing path {input_bq_uri.path} to view {view_bq_uri.path}")
     view_bq_uri.save_sql_as_view(f"select * from `{input_bq_uri.path}` -- {inputs[0]['instance_ts_str']}")
