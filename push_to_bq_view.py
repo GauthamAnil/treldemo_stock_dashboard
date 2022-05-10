5a1c4c3e65501adb6d0330174a59dfcc2adc4af0
Gautham Anil <gautham.anil@gmail.com>
2022-05-10 19:38:27 +0000

    updating to support new args format. using get_args

--- push_to_bq_view.py
+++ push_to_bq_view.py
@@ -1,28 +1,21 @@
 import argparse, yaml
 from treldev.gcputils import BigQueryURI
+from treldev import get_args
         
 def main(inputs, outputs, schedule_instance_ts, credentials, **kwargs):
 
-    assert len(inputs) == 2 # Two inputs has to be provided. One to be pushed, and one the latest.
-    if len(set(v['uri'] for v in inputs)) == 2: # if they are different, do nothing. This way, we only push the latest.
+    assert len(inputs) == 1 # Only one input dataset class can be provided.
+    input_datasets = list(inputs.values())[0]
+    assert len(inputs_datasets) == 2 # This should have only two datasets. One to be pushed, and one the latest.
+    if len(set(v['uri'] for v in input_datasets)) == 2: # if they are different, do nothing. This way, we only push the latest.
         print("Not pushing path to view")
         return
 
-    input_bq_uri = BigQueryURI(inputs[0]['uri'])
-    view_bq_uri = BigQueryURI(outputs[0]['uri'])
+    input_bq_uri = BigQueryURI(inputs_datasets[0]['uri'])
+    view_bq_uri = BigQueryURI(list(outputs.values())[0][0]['uri'])
     print(f"Pushing path {input_bq_uri.path} to view {view_bq_uri.path}")
     view_bq_uri.save_sql_as_view(f"select * from `{input_bq_uri.path}` -- {inputs[0]['instance_ts_str']}")
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--_args")
-    parser.add_argument("--_creds")
-    args, _ = parser.parse_known_args()
-    
-    with open(args._args) as f:
-        args_contents = yaml.safe_load(f)
-    
-    with open(args._creds) as f:
-        credentials = yaml.safe_load(f)
-
-    main(credentials=credentials, **args_contents)
+    args = 
+    main(credentials=credentials, **get_args())
