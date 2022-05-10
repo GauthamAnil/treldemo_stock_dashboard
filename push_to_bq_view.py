f5a9f7e83f0f926e5d5b365ee629ab7825d63253
Gautham Anil <gautham.anil@gmail.com>
2022-05-10 19:39:25 +0000

    updating to support new args format. using get_args

--- push_to_bq_view.py
+++ push_to_bq_view.py
@@ -17,5 +17,4 @@ def main(inputs, outputs, schedule_instance_ts, credentials, **kwargs):
     view_bq_uri.save_sql_as_view(f"select * from `{input_bq_uri.path}` -- {inputs[0]['instance_ts_str']}")
 
 if __name__ == '__main__':
-    args = 
     main(credentials=credentials, **get_args())
